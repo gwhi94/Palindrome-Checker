525f437ca1b48cd1c89517f44ff90d39991e6d42
George Whiting <gwhi94@gmail.com>
2018-09-21 16:48:42 +0100

    Js written and form built

---
+++ js.js
@@ -0,0 +1,50 @@
+function palindrome() {
+
+  var str = document.getElementById("word").value;
+  //gather users word input
+
+  var strippedString = str.replace(/[\W_]+/g,"");
+  //strips non alphaNumerical characters
+  var lowerCaseString = strippedString.toLowerCase(); 
+  //Turns str to lower case
+  var splitString = lowerCaseString.split(""); 
+  //return to new array
+  var reversedString = splitString.reverse();
+  //reverses array
+  var joinedString = reversedString.join("");
+  //joins array
+  
+  var comparisonChamberOne = [];
+  //array for original string
+  var comparisonChamberTwo = [];
+  //array for reversed string to compare to original
+
+  for (var i = 0;i < lowerCaseString.length;i++){
+    comparisonChamberOne.push(lowerCaseString[i]);
+  }
+  //pushes original string to array
+
+  for (var x = 0;x < joinedString.length;x++){
+    comparisonChamberTwo.push(joinedString[x]);
+  }
+  //pushes reversed string to array
+
+  if (comparisonChamberOne.length === comparisonChamberTwo.length &&
+    comparisonChamberOne.every((item, index) => comparisonChamberTwo[index] === item)){
+      console.log("true");
+  }else{
+      console.log("False");
+  }
+
+  //checks array items against each other for equality
+
+
+  return true;
+}
+
+
+
+
+
+
+
